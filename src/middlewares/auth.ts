d091b6dcc7d44d56992900f2673ae6240824f3d1
guihRovetta <guilherme020213@hotmail.com>
2020-09-09 01:58:29 -0300

    ✨ finish code of authenticate routes & tests

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -4,11 +4,15 @@ import AuthService from '@src/services/auth';
 
 export function authMiddleware(
   req: Partial<Request>,
-  _: Partial<Response>,
+  res: Partial<Response>,
   next: NextFunction
 ): void {
   const token = req.headers?.['x-access-token'];
-  const decoded = AuthService.decodeToken(token as string);
-  req.decoded = decoded;
-  next();
+  try {
+    const decoded = AuthService.decodeToken(token as string);
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    res.status?.(401).send({ code: 401, error: err.message });
+  }
 }
