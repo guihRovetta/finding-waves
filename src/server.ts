fd41216a6d40a821159e2ed86ef96931eb190a90
guihRovetta <guilherme020213@hotmail.com>
2020-09-14 23:21:09 -0300

    📝 add documentation with openapi & swagger

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,16 @@ import bodyParser from 'body-parser';
 import { Application } from 'express';
 import expressPino from 'express-pino-logger';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import { OpenApiValidator } from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
 
 import * as database from '@src/database';
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
+import apiSchema from './api.schema.json';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -19,6 +23,7 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    await this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
   }
@@ -47,6 +52,15 @@ export class SetupServer extends Server {
       beachesController,
       usersController,
     ]);
+  }
+
+  private async docsSetup(): Promise<void> {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
+    await new OpenApiValidator({
+      apiSpec: apiSchema as OpenAPIV3.Document,
+      validateRequests: true,
+      validateResponses: true,
+    }).install(this.app);
   }
 
   private async databaseSetup(): Promise<void> {
--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -30,18 +30,18 @@ describe('Users functional tests', () => {
       );
     });
 
-    it('Should return 422 when there is a validation error', async () => {
+    it('Should return a validation error', async () => {
       const newUser = {
         email: 'john@mail.com',
         password: '1234',
       };
       const response = await global.testRequest.post('/users').send(newUser);
 
-      expect(response.status).toBe(422);
+      expect(response.status).toBe(400);
       expect(response.body).toEqual({
-        code: 422,
+        code: 400,
         message: 'User validation failed: name: Path `name` is required.',
-        error: 'Unprocessable Entity',
+        error: 'Bad Request',
       });
     });
 
